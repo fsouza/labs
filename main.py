6b8ea78ac7e4894466183dc6fb6c8411ed17484b
Francisco Souza <francisco@franciscosouza.net>
2010-08-04 18:51:10 -0300

    Fixed the mai script

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 lib_dir = os.path.join(root_dir, 'lib')
-sys.path.append(lib_dir)
+sys.path.insert(0, lib_dir)
 
 from google.appengine.ext.webapp.util import run_wsgi_app
 from labs import app
