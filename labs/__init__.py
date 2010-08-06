a06fe40c53bf4e903e58d4dd03ae75462cfc6b49
Francisco Souza <francisco@franciscosouza.net>
2010-08-06 02:13:17 -0300

    Using settings.py file for settings

--- labs/__init__.py
+++ labs/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask
 from labs.views.admin import admin
 from labs.views.frontend import frontend
 
+import settings
+
 app = Flask('labs')
+app.config.from_object('labs.settings')
 app.register_module(admin, url_prefix = '/admin')
 app.register_module(frontend)
 import context_processors
