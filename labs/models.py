c8d302a199163937e7173da4765a8444882cb564
Francisco Souza <francisco@franciscosouza.net>
2010-08-06 21:57:26 -0300

    Overrided put method for slug generation

--- labs/models.py
+++ labs/models.py
@@ -1,10 +1,25 @@
 from google.appengine.ext import db
+from labs.util import slugify
 
 class Project(db.Model):
     name = db.StringProperty(required=True)
     slug = db.StringProperty()
     github_url = db.StringProperty()
     documentation_url = db.StringProperty()
+
+    def put(self):
+        if not self.slug:
+            slug = slugify(self.name)
+            new_slug = slug
+            counter = 1
+
+            while Project.all().filter('slug = ', new_slug).count() > 0:
+                new_slug = '%s-%d' %(slug, counter)
+                counter += 1
+
+            self.slug = new_slug
+
+        return super(Project, self).put();
 
 class Page(db.Model):
     title = db.StringProperty(required=True)
