9a0eb0999a40c8cd555698964ef8348001e10b7b
Francisco Souza <francisco@franciscosouza.net>
2010-08-06 21:57:37 -0300

    Saving a project on the database

--- labs/views/admin.py
+++ labs/views/admin.py
@@ -1,4 +1,4 @@
-from flask import Module, render_template
+from flask import Module, render_template, flash, redirect, url_for
 from labs.models import Project
 from labs.forms import ProjectForm
 
@@ -20,4 +20,13 @@ def new_project():
 
 @admin.route('/projects', methods=['POST'])
 def create_project():
-    return 'Hello'
+    form = ProjectForm()
+    if form.validate_on_submit():
+        project = Project(name = form.name.data,
+                          github_url = form.github_url.data,
+                          documentation_url = form.documentation_url.data
+                  )
+        project.put()
+        flash('Project saved on the database')
+        return redirect(url_for('list_projects'))
+    return render_template('admin/projects/new.html', form=form)
