fe6c505ba435bb044966a9c7f2b26ea3af274d25
Francisco Souza <francisco@franciscosouza.net>
2010-08-14 13:04:31 -0300

    Added select box to the form

--- labs/forms.py
+++ labs/forms.py
@@ -2,9 +2,14 @@ from flaskext import wtf
 from flaskext.wtf import validators
 
 class ProjectForm(wtf.Form):
-    name = wtf.TextField('Name', validators=[validators.Required()])
-    github_url = wtf.TextField('Github URL')
-    documentation_url = wtf.TextField('Documentation URL')
+    name = wtf.TextField(u'Name', validators=[validators.Required()])
+    programming_language = wtf.SelectField(u'Programming language', validators=[validators.Required()])
+    github_url = wtf.TextField(u'Github URL')
+    documentation_url = wtf.TextField(u'Documentation URL')
+
+    def set_programming_languages_options(self, languages):
+        """Set the programming languages options"""
+        self.programming_language.options = [(l.slug, l.name) for l in languages]
 
 class LanguageForm(wtf.Form):
-    name = wtf.TextField('Name', validators=[validators.Required()])
+    name = wtf.TextField(u'Name', validators=[validators.Required()])
