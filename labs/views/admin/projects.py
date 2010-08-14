2c0185d7af80de4e408e863bbc6f77f7d23840d0
Francisco Souza <francisco@franciscosouza.net>
2010-08-14 13:14:19 -0300

    Saving a project with a programming language on the database

--- labs/views/admin/projects.py
+++ labs/views/admin/projects.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for
-from labs.models import Project
+from labs.models import Project, ProgrammingLanguage
 from labs.forms import ProjectForm
 from labs.views.admin import admin
 
@@ -10,16 +10,22 @@ def list_projects():
 
 @admin.route('/projects/new')
 def new_project():
+    languages = ProgrammingLanguage.all()
     form = ProjectForm()
+    form.set_programming_languages_choices(languages)
     return render_template('admin/projects/new.html', form=form)
 
 @admin.route('/projects', methods=['POST'])
 def create_project():
     form = ProjectForm()
+    languages = ProgrammingLanguage.all()
+    form.set_programming_languages_choices(languages)
     if form.validate_on_submit():
+        language = ProgrammingLanguage.all().filter('slug =', form.programming_language.data).get()
         project = Project(name = form.name.data,
                           github_url = form.github_url.data,
-                          documentation_url = form.documentation_url.data
+                          documentation_url = form.documentation_url.data,
+                          language = language
                   )
         project.put()
         flash('Project saved on the database')
