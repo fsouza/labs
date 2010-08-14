2c131d12d5f73128372ddb5f6fc676d8f83b0bb7
Francisco Souza <francisco@franciscosouza.net>
2010-08-14 19:33:39 -0300

    More tests

--- tests/unit/test_util.py
+++ tests/unit/test_util.py
@@ -10,3 +10,19 @@ def test_slugify_portuguese():
     "Should slugify a portuguese name"
     from labs.util import slugify
     assert_equals(slugify(u'Francisco Antônio da Silva Souza.'), u'francisco-antonio-da-silva-souza')
+
+def test_slug_generation_for_model():
+    "Should generate a slug for a model"
+    from labs.models import Project
+    from labs.util import generate_slug_field
+    assert_equals(generate_slug_field(Project, u'Undefined project'), u'undefined-project')
+
+def test_slug_generation_for_model_repeated():
+    "Should generate a slug with counter for a model when the slug already exists"
+    language_name = u'Ruby'
+    expected_slug = u'ruby-1'
+    from labs.models import ProgrammingLanguage
+    from labs.util import generate_slug_field
+    language = ProgrammingLanguage(name = language_name)
+    language.put()
+    assert_equals(generate_slug_field(ProgrammingLanguage, language_name), expected_slug)
