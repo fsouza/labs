fbb84766c7b58bd13414e009678f758fb2c453b0
Francisco Souza <francisco@franciscosouza.net>
2010-08-14 12:32:32 -0300

    Updated test

--- tests/test_views_admin.py
+++ tests/test_views_admin.py
@@ -1,10 +1,12 @@
 import unittest
 import mocker
+import labs
 
 class TestAdmin(unittest.TestCase):
 
     def setUp(self):
         self.mocker = mocker.Mocker()
+        self.app = labs.app.test_client()
 
     def tearDown(self):
         self.mocker.restore()
