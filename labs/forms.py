f8c478a5125f632cffdf873a3a482cdfb73e91dc
Francisco Souza <francisco@franciscosouza.net>
2010-09-11 20:11:16 -0300

    Updated ProjectForm for using an auto-generated form, exploring the App Engine extension for WTForms

--- labs/forms.py
+++ labs/forms.py
@@ -1,27 +1,24 @@
 from flaskext import wtf
-from flaskext.wtf import validators
 from labs.models import ProgrammingLanguage, Project
 from wtforms.ext.appengine.db import model_form
 
-class ProjectForm(wtf.Form):
-    name = wtf.TextField(u'Name', validators=[validators.Required()])
-    programming_language = wtf.SelectField(u'Programming language', validators=[validators.Required()])
-    github_url = wtf.TextField(u'Github URL')
-    documentation_url = wtf.TextField(u'Documentation URL')
+BaseProjectForm = model_form(Project, base_class = wtf.Form, exclude = ['slug'])
 
-    def __init__(self, programming_language_choices, model_instance = None, *args, **kwargs):
+class ProjectForm(BaseProjectForm):
+    def __init__(self, model_instance = None, *args, **kwargs):
         super(ProjectForm, self).__init__(*args, **kwargs)
         self.model = None
-        self.set_programming_languages_choices(programming_language_choices)
         if model_instance:
             self.name.data = model_instance.name
-            self.programming_language.data = model_instance.language.slug
+            self.language.data = model_instance.language.slug
             self.github_url.data = model_instance.github_url
             self.documentation_url.data = model_instance.documentation_url
             self.model = model_instance
+        self.language.label_attr = 'name'
+        self.language.query.order('name')
 
     def save(self):
-        language = ProgrammingLanguage.all().filter('slug =', self.programming_language.data).get()
+        language = ProgrammingLanguage.get(self.language.data)
         if self.model:
             self.model.name = self.name.data
             self.model.github_url = self.github_url.data
@@ -36,10 +33,5 @@ class ProjectForm(wtf.Form):
                         )
         self.model.put()
         return self.model
-
-    def set_programming_languages_choices(self, languages):
-        """Set the programming languages options"""
-        self.programming_language.choices = [(l.slug, l.name) for l in languages]
-        self.programming_language.choices.insert(0, ('', 'Select the language'))
 
 LanguageForm = model_form(ProgrammingLanguage, base_class = wtf.Form, exclude = ['slug'])
